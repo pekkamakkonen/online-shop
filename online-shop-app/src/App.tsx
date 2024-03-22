a8db37d34e77899210227ca367d3239633c94e19
AB4546 <AB4546@student.jamk.fi>
2024-03-22 02:23:01 +0200

    Added Routes

--- online-shop-app/src/App.tsx
+++ online-shop-app/src/App.tsx
@@ -1,11 +1,22 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+// import './App.css';
+import Checkout from './shopping/components/Checkout/Checkout';
+import MyAccount from './shopping/components/MyAccount/MyAccount';
+import ProductDetail from './shopping/components/ProductDetail/ProductDetail';
 import ProductList from './shopping/components/ProductList/ProductList';
+import ShoppingCart from './shopping/components/ShoppingCart/ShoppingCart';
 
 function App() {
   return (
     <>
       <h1>Online Shop</h1>
-      <ProductList />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/my-account" element={<MyAccount />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+      </Routes>
     </>
   );
 }
--- online-shop-app/src/shopping/components/ProductDetail/ProductDetail.tsx
+++ online-shop-app/src/shopping/components/ProductDetail/ProductDetail.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const ProductDetail = () => {
   return <div>ProductDetail</div>;
 };
