f59777dc6c688136380c4b78319762729983ce09
AB4546 <AB4546@student.jamk.fi>
2024-03-31 01:43:58 +0200

    Added SQL command for creating of Customer table.

---
+++ online-shop-db/create-tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Customer (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(200) UNIQUE NOT NULL,
+    Phone VARCHAR(20) NOT NULL,
+    StreetAddress VARCHAR(50) NOT NULL,
+    PostalCode VARCHAR(15) NOT NULL,
+    City VARCHAR(30) NOT NULL,
+    Country CHAR(2) NOT NULL,
+    Username VARCHAR(50) UNIQUE NOT NULL,
+    Password VARCHAR(255) NOT NULL
+);
