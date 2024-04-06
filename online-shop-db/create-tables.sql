68c4031bdd979181a4f69fb3e0e9aece02b07713
AB4546 <AB4546@student.jamk.fi>
2024-04-06 17:12:08 +0300

    Added Product table

--- online-shop-db/create-tables.sql
+++ online-shop-db/create-tables.sql
@@ -1,3 +1,5 @@
+USE OnlineShop;
+
 CREATE TABLE Customer (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     FirstName VARCHAR(50) NOT NULL,
@@ -7,7 +9,18 @@ CREATE TABLE Customer (
     StreetAddress VARCHAR(50) NOT NULL,
     PostalCode VARCHAR(15) NOT NULL,
     City VARCHAR(30) NOT NULL,
-    Country CHAR(2) NOT NULL,
+    Country CHAR(2) NOT NULL DEFAULT 'FI', -- Hardcoded value, not need ask info in frontend
     Username VARCHAR(50) UNIQUE NOT NULL,
     Password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE Product (
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL,
+	Description VARCHAR(1000),
+	ImageUrl VARCHAR(255),
+	UnitPrice DECIMAL(6,2) NOT NULL CHECK (UnitPrice >= 0),
+	UnitsInStock SMALLINT NOT NULL CHECK (UnitsInStock >= 0),
+	VatPercentage TINYINT NOT NULL CHECK (VatPercentage BETWEEN 0 AND 100)
+);
+	
